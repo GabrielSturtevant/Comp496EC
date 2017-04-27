e5b4236898275d93052a8aa775bb499249f4080e
Gabriel.Sturtevant <gsturtevant87@gmail.com>
2017-04-26 19:44:27 -0700

    iuhjg

--- src/EC2.java
+++ src/EC2.java
@@ -7,8 +7,6 @@ public class EC2 {
     int[] ans_info;
 
     public static void main(String[] args) {
-        System.out.println(power_of_n(5));
-        System.exit(1);
         int W = 100;
         int[] weights = {9,16,12,8,7, 16,7,8,9,14, 15,19,22,2,4,  5,10,11,3,17 ,15,18,15,9,7 };
         int[] benefits = {1,7,3,4,5,  5,3,4,6,2,   6,6,4,2,1,  3,12,2,4,5,   4,3,2,1,3};
@@ -17,17 +15,14 @@ public class EC2 {
 
         int finalWeight = 0;
         int finalValue = 0;
-        String toPrint;
         for (int i = 0; i < instance.length; i++) {
             if(instance.ans_info[i] == 1){
-                toPrint = i + ", Weight: " + instance.weights[i];
-                toPrint += ", Value: " + instance.benefits[i];
-                System.out.println(toPrint);
+                System.out.print(i + " ");
                 finalWeight += instance.weights[i];
                 finalValue += instance.benefits[i];
             }
         }
-        System.out.println("Final value: " + finalValue);
+        System.out.println("\nFinal value: " + finalValue);
         System.out.println("Final weight: " + finalWeight);
     }
 
